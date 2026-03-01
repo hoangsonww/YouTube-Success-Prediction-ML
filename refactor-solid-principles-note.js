0f5fdcd3052e7b71c5b647946a047d45cada8a1c
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:08 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'YouTube-Success-Prediction-ML';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
